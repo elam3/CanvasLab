bb562210eee169e3ada6428a3f05475974080d1a
Edison L <elam3@mail.ccsf.edu>
2017-03-15 20:30:26 -0700

    Milestone 2: Build a Simple Draggable Tray

--- smilies/ViewController.swift
+++ smilies/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,30 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parentView
+        let location = sender.location(in: self.view)
+        
+         //= trayView.center
+        
+        if (sender.state == UIGestureRecognizerState.began) {
+            NSLog("Gesture began at: %@", NSStringFromCGPoint(location))
+            
+            trayOriginalCenter = trayView.center
+            
+        } else if (sender.state == UIGestureRecognizerState.changed) {
+            //NSLog("Gesture changed at: %@", NSStringFromCGPoint(location))
+            
+            let translationY = sender.translation(in: trayView).y
+            
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translationY)
+            
+        } else if (sender.state == UIGestureRecognizerState.ended) {
+            NSLog("Gesture ended at: %@", NSStringFromCGPoint(location))
+
+        }
     }
 
 
